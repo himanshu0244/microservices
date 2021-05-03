cbc98764110d9fad443f6c79460aece0fba75a2c
himanshu0244 <42444625+himanshu0244@users.noreply.github.com>
2021-05-03 15:39:14 +0530

    microservices project

---
+++ src/main/java/com/codet/microservies/MicroserviesApplication.java
@@ -0,0 +1,13 @@
+package com.codet.microservies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codet/microservies/MicroserviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codet.microservies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
