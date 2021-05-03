6c2cbe6c37075d602e73429d8d10c2204a8ee147
himanshu0244 <42444625+himanshu0244@users.noreply.github.com>
2021-05-03 16:21:18 +0530

    adding user service to handle basic CRUD operations

---
+++ src/main/java/com/codet/microservices/user/User.java
@@ -0,0 +1,19 @@
+package com.codet.microservices.user;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	private Integer userId;
+	private String name;
+	private Date dob;
+}
