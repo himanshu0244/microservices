22b8b8a9ba4030b5f99397e2ff47b048136c7592
himanshu0244 <42444625+himanshu0244@users.noreply.github.com>
2021-05-04 13:52:35 +0530

    added validations and custom exception handler

--- src/main/java/com/codet/microservices/user/User.java
+++ src/main/java/com/codet/microservices/user/User.java
@@ -2,6 +2,10 @@ package com.codet.microservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +18,12 @@ import lombok.Setter;
 public class User {
 
 	private Integer userId;
+
+	@Size(min = 2, message = "name should have atleast 2 characters")
+	@NotNull(message = "name can not be null")
 	private String name;
+
+	@Past
+	@NotNull(message = "birth date can not be null")
 	private Date dob;
 }
